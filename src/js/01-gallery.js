53ac3bdf5e323d1773d86ed9d35b03987fbea550
akoval25 <88685936+akoval25@users.noreply.github.com>
2022-01-29 18:18:01 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryCards = createGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryCards);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    `;
+  })
+  .join('');
+}
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+  const isGalleryItem = evt.target.classList.contains('gallery__image');
+  if (!isGalleryItem) {
+    return;
+  }
+
+   
+    var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+}
