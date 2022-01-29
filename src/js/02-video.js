9ae8d6966fe5ec40a4962d91e73c6c21cc699d0a
akoval25 <88685936+akoval25@users.noreply.github.com>
2022-01-29 20:33:44 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+
+function onTimeupdate(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
