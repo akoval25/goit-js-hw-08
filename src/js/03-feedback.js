9fc834c08cc8f4034ea27b87e347c8d13e8046c9
akoval25 <88685936+akoval25@users.noreply.github.com>
2022-01-29 21:09:37 +0200

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+const textareaRef = document.querySelector('.feedback-form textarea');
+const inputRef = document.querySelector('.feedback-form input');
+
+const formData = {};
+
+formRef.addEventListener('submit', onSubmit);
+formRef.addEventListener('input', throttle(onInput, 500));
+
+inputText();
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  if (inputRef.value && textareaRef.value) {
+    console.log({
+      email: inputRef.value,
+      message: textareaRef.value,
+    });
+
+    e.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state');
+  }
+}
+
+function onInput(e) {
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function inputText() {
+  const text = localStorage.getItem('feedback-form-state');
+
+  if (text) {
+    inputRef.value = JSON.parse(text).email;
+    textareaRef.value = JSON.parse(text).message;
+  }
+}
